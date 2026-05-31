6a1057c50d728f7692c150e738dee6af68a23d25
Moch. Raditya Wiryateja <radityawiryateja62@gmail.com>
2026-05-31 09:47:53 +0700

    Change radar kit name and enhance notification text
    Updated the radar kit name and added a notification message.

--- bot_heroku.py
+++ bot_heroku.py
@@ -669,8 +669,9 @@ async def handle_pesan(update: Update, context: CallbackContext):
 
     if text_content == "📡 Radar Incaran":
         return await update.message.reply_text(
-            "📡 *Smart Radar Kitheons*\n\n"
-            "Pilih menu di bawah ini untuk mengatur radar incaranmu:", 
+            "📡 *Smart Radar Bazarfess*\n\n"
+            "Pilih menu di bawah ini untuk mengatur radar incaranmu:",
+            "\nBot akan otomatis memberikan notifikasi untuk setiap menfess yang sesuai",
             parse_mode="Markdown",
             reply_markup=get_radar_keyboard()
         )
